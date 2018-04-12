7cca99112573a71cf7ec4f8ef3f9530f48eb84cb
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-12 13:05:36 -0500

    Se crea un TextInputLayout y en la clase se crea el método Listener cuando cambia el texto

--- app/src/main/java/mx/com/webmaps/md_ejercicio3/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio3/MainActivity.java
@@ -2,12 +2,39 @@ package mx.com.webmaps.md_ejercicio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatEditText user;
+    AppCompatEditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = (AppCompatEditText) findViewById(R.id.username_TextField);
+        pass = (AppCompatEditText) findViewById(R.id.password_TextField);
+
+        user.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
     }
 }
